c239bddf459d5076388731309a66017a36cec719
Nicolas L <lorion06@gmail.com>
2022-01-07 16:04:37 +0100

    Display score

--- snake_script.js
+++ snake_script.js
@@ -1,7 +1,4 @@
-    // Score
-    let score = document.getElementById('score');
-
-
+    let score = 0;
     // Snake color
     const snakeColColor = 'lightblue';
     const snakeBoardColor = 'green';
@@ -80,13 +77,27 @@
         snakeBoardCtx.strokeRect(snakePart.x, snakePart.y, 10, 10);
     }
 
+    function hasGameEnded() {
+        for(let i = 4; i < snake.length; i++) {
+            if(snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true;
+        }
+            const hitLeftWall = snake[0].x < 0;
+            const hitRightWall = snake[0].x > snakeBoard.width - 10;
+            const hitTopWall = snake[0].y < 0;
+            const hitBottomWall = snake[0].y > snakeBoard.width - 10;
+
+            const hitWall = hitLeftWall || hitRightWall || hitTopWall || hitBottomWall;
+
+            return hitWall;
+    }
+
     function changeDirection(e) {
         const LEFT_KEY = 37;
         const RIGHT_KEY = 39;
         const UP_KEY = 38;
         const DOWN_KEY = 40;
 
-        if(changeDirection) return;
+        if(changingDirection) return;
         changingDirection = true;
         const keyPressed = e.keyCode;
         const goingUp = dy === -10;
@@ -115,19 +126,7 @@
         }
     }    
 
-    function hasGameEnded() {
-        for(let i = 4; i < snake.length; i++) {
-            if(snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true;
-        }
-            const hitLeftWall = snake[0].x < 0;
-            const hitRightWall = snake[0].x > snakeBoard.width - 10;
-            const hitTopWall = snake[0].y < 0;
-            const hitBottomWall = snake[0].y > snakeBoard.width - 10;
-
-            const hitWall = hitLeftWall || hitRightWall || hitTopWall || hitBottomWall;
-
-            return hitWall;
-    }
+    
  
     function moveSnake() {
         const head = {x: snake[0].x + dx, y : snake[0].y + dy};
@@ -135,7 +134,8 @@
         snake.unshift(head);
         const hasEatenFood = snake[0].x === foodX && snake[0].y === foodY;
         if(hasEatenFood) {
-            score.innerHTML +=10;
+            score += 10;
+            document.getElementById('score').innerText = score;
             genFood();
         } else {
             snake.pop()
@@ -147,8 +147,8 @@
     }
 
     function genFood() {
-        foodX = randomFood(0, snakeBoardCtx.width - 10);
-        foodY = randomFood(0, snakeBoardCtx.height - 10);
+        foodX = randomFood(0, snakeBoard.width - 10);
+        foodY = randomFood(0, snakeBoard.height - 10);
         snake.forEach(function hasSnakeEatenFood(part) {
             const hasEaten = part.x == foodX && part.y == foodY;
             if (hasEaten) genFood()
