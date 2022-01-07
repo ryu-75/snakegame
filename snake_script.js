1161823a561e15260c4bbea4d9352d5e11889d4b
Nicolas L <lorion06@gmail.com>
2022-01-07 14:47:23 +0100

    Initialize variable and display canvas

---
+++ snake_script.js
@@ -0,0 +1,51 @@
+
+    // Snake color
+    const snakeColColor = 'lightblue';
+    const snakeBoardColor = 'green';
+
+    // Canvas color
+    const canvasColColor = 'lightgray';
+    const canvasBoardColor = 'black';
+
+    const foodColColor = 'orange';
+    const foodBoardColor = 'red';
+
+    // Canvas
+    const snakeBoard = document.getElementById('snakeboard');
+    const snakeBoardCtx = snakeBoard.getContext('2d');
+
+    const snake = [
+        {x : 200, y : 200},
+        {x : 190, y : 200},
+        {x : 180, y : 200},
+        {x : 170, y : 200},
+        {x : 160, y : 200},
+    ];
+
+    let changingDirection = false;
+
+
+    function main() {
+        
+        changingDirection = false;
+        setTimeout(function onTick()
+        {
+            clearCanvas();
+            main();
+        })
+    }
+
+    // Create a canvas
+    function clearCanvas() {
+        // Add background color to canvas
+        snakeBoardCtx.fillStyle = canvasColColor;
+        // Add boarder color to canvas
+        snakeBoardCtx.strokestyle = canvasBoardColor;
+        // Fill entirely canvas
+        snakeBoardCtx.fillRect(0, 0, snakeBoard.width, snakeBoard.height);
+        // Draw a border 
+        snakeBoardCtx.strokeRect(0, 0, snakeBoard.width, snakeBoard.height);
+       
+    }
+    clearCanvas()
+
